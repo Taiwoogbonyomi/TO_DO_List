ca0015535f69a736c7ea2cdaf6c0241794e3799a
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-06-20 00:30:53 +0000

    fix bug

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ SCOPE = [
     ]
 
 
-CREDS = Credentials.from_service_account_file("creds.json")
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 spreadsheet = GSPREAD_CLIENT.open('To-Do List')
